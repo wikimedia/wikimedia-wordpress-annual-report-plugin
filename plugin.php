68442a7fed669f054c4125d363fefb9c47f6087e
goldenapples <goldenapplesdesign@gmail.com>
2024-01-31 08:46:41 -0500

    Initial commit for plugin structure

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Wikimedia WordPress Annual Report
+Description: WP plugin to house all the functionality required for building Wikimedia's digital-first annual reports.
+Author: Human Made
+Author URI: https://hmn.md
+Version: 0.1
+License: GPL-2+
+*/
+
