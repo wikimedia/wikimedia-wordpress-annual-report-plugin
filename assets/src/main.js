5614de1dc0d578d70df03f2b33ef5e2fb70351ae
Philip John <phil@philipjohn.me.uk>
2024-03-06 17:55:24 +0000

    Import the frontend JS/CSS for each block in main.js

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,3 +1,9 @@
 /**
  * Front end funcionality for report pages.
  */
+
+// For each block in the blocks directory, import it's view.js file.
+// This will ensure that the view.js file is included in the front end build.
+for ( const block of require.context( './blocks', true, /view\.js$/ ) ) {
+	require( block );
+}
