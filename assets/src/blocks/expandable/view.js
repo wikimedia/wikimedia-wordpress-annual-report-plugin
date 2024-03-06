e4866cbb493e4446126e255409cb517fcbdd6a8f
Philip John <phil@philipjohn.me.uk>
2024-03-06 17:54:30 +0000

    Expandable Block: Import styles and add event listener for the button

--- assets/src/blocks/expandable/view.js
+++ assets/src/blocks/expandable/view.js
@@ -19,6 +19,19 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
  */
 
+import './style.scss';
+
+// Add a click event listener to each expandable block.
+// When the button is clicked, toggle the is-expanded class on the content.
+document.querySelectorAll( '.wp-block-wikimedia-annual-report-expandable' ).forEach( ( block ) => {
+	const button = block.querySelector( '.expandable-button' );
+	const content = block.querySelector( '.expandable-content' );
+
+	button.addEventListener( 'click', () => {
+		content.classList.toggle( 'is-expanded' );
+	} );
+} );
+
 /* eslint-disable no-console */
 console.log( 'Hello World! (from create-block-wikimedia-ar-expandable block)' );
 /* eslint-enable no-console */
