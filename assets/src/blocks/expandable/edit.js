0366ce589720959f7b82df662a3a22add50c0e8e
Philip John <phil@philipjohn.me.uk>
2024-03-06 15:37:48 +0000

    Initial basics of Expandable Block

---
+++ assets/src/blocks/expandable/edit.js
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, InnerBlocks, BlockControls, RichText } from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Block edit function.
+ * @returns {Element} Element to render.
+ */
+export default function Edit( { attributes, setAttributes } ) {
+
+	const [ expanded, setExpanded ] = useState( true );
+
+	const className = ( expanded ) ? 'expandable-content is-expanded' : 'expandable-content is-collapsed';
+
+	return (
+		<div { ...useBlockProps() }>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton
+						icon={ expanded ? 'arrow-up-alt2' : 'arrow-down-alt2' }
+						title={ expanded ? __( 'Collapse' ) : __( 'Expand' ) }
+						onClick={ () => setExpanded( ! expanded ) }
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+
+			<div
+				className={ className }
+				data-visible-amount={ attributes.visibleAmount }
+				data-visible-unit={ attributes.visibleUnit }
+			>
+				<InnerBlocks />
+			</div>
+
+			<div className="expandable-expander">
+				<RichText
+					tagName="span"
+					className="expandable-linktext"
+					value={ attributes.linkText }
+					placeholder='Read more...'
+					onChange={ ( content ) => setAttributes( { linkText: content } ) }
+					withoutInteractiveFormatting
+				/>
+			</div>
+
+		</div>
+	);
+
+}
--- assets/src/main.js
+++ assets/src/main.js
@@ -1,3 +1,38 @@
 /**
  * Front end funcionality for report pages.
  */
+
+/**
+ * Autoload and require all block editor functionality.
+ */
+import { autoloadBlocks, autoloadPlugins } from 'block-editor-hmr';
+import { registerBlockCollection } from '@wordpress/blocks';
+
+// Bundle all the Annual Report blocks into a single collection.
+registerBlockCollection(
+	'wikimedia-annual-report',
+	{
+		title: 'Annual Report',
+		icon: 'chart-bar',
+	}
+);
+
+// Load all block index files.
+autoloadBlocks(
+	{ getContext: () => require.context( './blocks', true, /index\.js$/ ) },
+	( context, loadModules ) => {
+		if ( module.hot ) {
+			module.hot.accept( context.id, loadModules );
+		}
+	}
+);
+
+// Load all plugin index files.
+// autoloadPlugins(
+// 	{ getContext: () => require.context( './plugins', true, /index\.js$/ ) },
+// 	( context, loadModules ) => {
+// 		if ( module.hot ) {
+// 			module.hot.accept( context.id, loadModules );
+// 		}
+// 	}
+// );
