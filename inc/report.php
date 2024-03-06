d64ea054594d0195bf1d6f2f49068603c0a6bbc0
Philip John <phil@philipjohn.me.uk>
2024-03-06 15:50:39 +0000

    Ensure our blocks are allowed in the editor

--- inc/report.php
+++ inc/report.php
@@ -10,6 +10,7 @@ namespace WMF\Reports\Report;
  */
 function bootstrap() {
 	add_action( 'init', __NAMESPACE__ . '\\register_post_type' );
+	add_filter( 'allowed_block_types_all', __NAMESPACE__ . '\\allowed_block_types', 11, 2 );
 }
 
 /**
@@ -34,4 +35,29 @@ function register_post_type() {
 			],
 		]
 	);
+}
+
+/**
+ * Filter the allowed blocks to ensure all our Annual Report blocks are included.
+ *
+ * @param bool|string[]            $allowed_block_types  Array of block type slugs, or boolean to enable/disable all.
+ * @param \WP_Block_Editor_Context $block_editor_context The current block editor context.
+ *
+ * @return bool|string[] Filtered allowed blocks list.
+ */
+function allowed_block_types( $allowed_block_types, $block_editor_context ) {
+
+	// Ensure all our blocks are allowed and not excluded by the theme.
+	$ar_blocks = [
+		'expandable'
+	];
+
+	foreach ( $ar_blocks as $block ) {
+		$block_name = 'wikimedia-annual-report/' . $block;
+		if ( ! in_array( $block_name, $allowed_block_types, true ) ) {
+			$allowed_block_types[] = $block_name;
+		}
+	}
+
+	return $allowed_block_types;
 }
