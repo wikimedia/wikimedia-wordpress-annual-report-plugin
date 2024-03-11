449cf4a8cc40a53c5a0b094c36de41a8663e1137
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-11 17:03:05 +0800

    Rewire Expandable block to use modern block.json pattern

--- src/frontend.js
+++ src/frontend.js
@@ -1,9 +1,3 @@
 /**
  * Front end funcionality for report pages.
  */
-
-// For each block in the blocks directory, import it's view.js file.
-// This will ensure that the view.js file is included in the front end build.
-for ( const block of require.context( './blocks', true, /view\.js$/ ) ) {
-	require( block );
-}
