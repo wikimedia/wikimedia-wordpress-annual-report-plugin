4f9ebf824154ab5a9e10e86cdbfed961afbab794
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-11 16:20:41 +0800

    945: Introduce hot-reloading helpers and add to example block

--- src/blocks/example-dynamic/index.js
+++ src/blocks/example-dynamic/index.js
@@ -31,3 +31,11 @@ registerBlockType( metadata.name, {
 	 */
 	edit: Edit,
 } );
+
+// Block HMR boilerplate.
+if ( module.hot ) {
+	module.hot.accept();
+	const { deregister, refresh } = require( '../../helpers/hot-blocks.js' );
+	module.hot.dispose( deregister( metadata.name ) );
+	refresh( metadata.name, module.hot.data );
+}
