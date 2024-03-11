da463f077a769cc51abbe7ba00fbefa7d011c6f0
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-11 20:06:46 +0800

    Expander block: continue implementation
    - Updates CSP fix for wp-scripts
    - Introduces frontend and editor styling for button
    - Edits saved markup
    - Simplifies shape of block with useInnerBlockProps
    - Begin implementing custom color picker
    - Implement height control in editor

--- src/editor.js
+++ src/editor.js
@@ -3,6 +3,8 @@
  */
 import { registerBlockCollection } from '@wordpress/blocks';
 
+import './editor.scss';
+
 // Bundle all the Annual Report blocks into a single collection.
 registerBlockCollection( 'wikimedia-annual-report', {
 	title: 'Annual Report',
