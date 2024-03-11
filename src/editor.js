6a9627e4995fc15b7d26f2a725f0b05301a127a5
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-11 16:50:38 +0800

    Reorganize block based on new structure from 945 (#8) and autoformat code

--- src/editor.js
+++ src/editor.js
@@ -1,39 +1,10 @@
 /*
  * Editor functionality.
  */
-
-/**
- * Autoload and require all block editor functionality.
- */
-import { autoloadBlocks } from 'block-editor-hmr';
-
 import { registerBlockCollection } from '@wordpress/blocks';
 
 // Bundle all the Annual Report blocks into a single collection.
-registerBlockCollection(
-	'wikimedia-annual-report',
-	{
-		title: 'Annual Report',
-		icon: 'chart-bar',
-	}
-);
-
-// Load all block index files.
-autoloadBlocks(
-	{ getContext: () => require.context( './blocks', true, /index\.js$/ ) },
-	( context, loadModules ) => {
-		if ( module.hot ) {
-			module.hot.accept( context.id, loadModules );
-		}
-	}
-);
-
-// Load all plugin index files.
-// autoloadPlugins(
-// 	{ getContext: () => require.context( './plugins', true, /index\.js$/ ) },
-// 	( context, loadModules ) => {
-// 		if ( module.hot ) {
-// 			module.hot.accept( context.id, loadModules );
-// 		}
-// 	}
-// );
+registerBlockCollection( 'wikimedia-annual-report', {
+	title: 'Annual Report',
+	icon: 'chart-bar',
+} );
