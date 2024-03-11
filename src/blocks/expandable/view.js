82fedb899ecc99c2b46e3b69e767209f541666e4
Philip John <phil@philipjohn.me.uk>
2024-03-11 13:56:01 +0000

    Expandable: Set the button background to white by default.

--- src/blocks/expandable/view.js
+++ src/blocks/expandable/view.js
@@ -9,6 +9,7 @@ document
 		const showmoretext = button.dataset.showmoretext;
 		const showlesstext = button.dataset.showlesstext;
 		const content = block.querySelector( '.expandable-content' );
+
 		// Get the visible amount and unit from the data attributes.
 		const visibleAmount = content.dataset.visibleAmount;
 		const visibleUnit = content.dataset.visibleUnit;
@@ -19,9 +20,10 @@ document
 		button.addEventListener( 'click', () => {
 			content.classList.toggle( 'is-expanded' );
 			const expanded = content.classList.contains( 'is-expanded' );
-console.log('expanded', expanded);
+
+			// Update the button text and content height.
 			content.setAttribute( 'aria-expanded', ! expanded );
 			content.setAttribute( 'style', `height: ${ expanded ? 'auto' : visibleAmount + visibleUnit }` );
-			button.innerText = expanded ? showlesstext : showlesstext;
+			button.innerText = expanded ? showlesstext : showmoretext;
 		} );
 	} );
