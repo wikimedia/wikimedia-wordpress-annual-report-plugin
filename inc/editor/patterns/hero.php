8008ba0aa6460e4120ae332e2440ceeebe506f26
Philip John <phil@philipjohn.me.uk>
2024-03-12 12:55:22 +0000

    Hero Pattern: Make image wide

--- inc/editor/patterns/hero.php
+++ inc/editor/patterns/hero.php
@@ -9,8 +9,8 @@ const NAME = 'wmf-reports/hero';
 
 const PATTERN = <<<CONTENT
 <!-- wp:group {"align":"full","className":"wmf-pattern-reports-hero","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull wmf-pattern-reports-hero"><!-- wp:image {"lock":{"move":true,"remove":true}} -->
-<figure class="wp-block-image"><img alt=""/></figure>
+<div class="wp-block-group alignfull wmf-pattern-reports-hero"><!-- wp:image {"align":"full","lock":{"move":true,"remove":true}} -->
+<figure class="wp-block-image alignfull"><img alt=""/></figure>
 <!-- /wp:image -->
 
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"className":"reports-hero-headings","layout":{"type":"constrained"}} -->
