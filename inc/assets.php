26025091519a4dd1fc9f4a3b09a358874efca106
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-12 15:45:26 +0800

    Expandable: Fix style loading issue on frontend

--- inc/assets.php
+++ inc/assets.php
@@ -96,7 +96,7 @@ function enqueue_frontend_assets() : void {
 
 	wp_enqueue_style(
 		'annual-report-plugin-frontend',
-		build_file_uri( 'style-frontend.css' ),
+		build_file_uri( 'frontend.css' ),
 		[
 			'dashicons'
 		]
