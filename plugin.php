8066a75ce48aef5b19a3ed9e79ee66abd1df2627
Philip John <phil@philipjohn.me.uk>
2024-03-12 12:07:05 +0000

    Reports Hero: Initially hero pattern, styling and font

--- plugin.php
+++ plugin.php
@@ -18,6 +18,8 @@ require_once __DIR__ . '/inc/asset-loader/namespace.php';
 require_once __DIR__ . '/inc/asset-loader/utilities.php';
 require_once __DIR__ . '/inc/blocks/expandable.php';
 require_once __DIR__ . '/inc/report.php';
+require_once __DIR__ . '/inc/editor/patterns.php';
+require_once __DIR__ . '/inc/editor/patterns/hero.php';
 
 Assets\bootstrap();
 Blocks\Expandable\bootstrap();
