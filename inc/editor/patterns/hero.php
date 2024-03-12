c3ad728d35a5ac07da1398ac5bdc53030c032c29
Philip John <phil@philipjohn.me.uk>
2024-03-12 12:49:22 +0000

    Hero Pattern: Style editor and lock pattern

--- inc/editor/patterns/hero.php
+++ inc/editor/patterns/hero.php
@@ -9,19 +9,21 @@ const NAME = 'wmf-reports/hero';
 
 const PATTERN = <<<CONTENT
 <!-- wp:group {"align":"full","className":"wmf-pattern-reports-hero","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull wmf-reports-hero"><!-- wp:image -->
+<div class="wp-block-group alignfull wmf-pattern-reports-hero"><!-- wp:image {"lock":{"move":true,"remove":true}} -->
 <figure class="wp-block-image"><img alt=""/></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading -->
+<!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"className":"reports-hero-headings","layout":{"type":"constrained"}} -->
+<div class="wp-block-group reports-hero-headings"><!-- wp:heading -->
 <h2 class="wp-block-heading">Annual Report 2023</h2>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":3} -->
 <h3 class="wp-block-heading">The people<br>behind a year<br>of impact around<br>the world</h3>
-<!-- /wp:heading -->
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
 
-<!-- wp:wmf-reports/expandable -->
+<!-- wp:wmf-reports/expandable {"lock":{"move":true,"remove":true}} -->
 <div class="wp-block-wmf-reports-expandable"><div class="expandable-content" data-visible-amount="300" data-visible-unit="px"><!-- wp:paragraph -->
 <p>This year’s Annual Report highlights the people who made sure that the Wikimedia projects made a big difference in the 2022-2023 fiscal year. If you’re a donor, that means you. If you’re a contributor to Wikipedia or any of the Wikimedia Foundation’s 12 other knowledge projects, that means you. Every year, millions and millions of people give selflessly to the ipsum duis consectetur aute eu duis cillum officia proident ea ut incididunt.</p>
 <!-- /wp:paragraph -->
