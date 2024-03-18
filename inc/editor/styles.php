972375010450bdc9311d09adbb6db061ea4c0924
K. Adam White <kadamwhite@users.noreply.github.com>
2024-03-18 19:34:03 +0800

    Block styles: Introduce block style for section heading text treatment

---
+++ inc/editor/styles.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Block styles for content editors to quickly vary the appearance of existing blocks.
+ */
+
+namespace WMF\Reports\Editor\Styles;
+
+/**
+ * Hook into WordPress
+ */
+function bootstrap() {
+	add_action( 'after_setup_theme', __NAMESPACE__ . '\\register_styles' );
+}
+
+/**
+ * Register our styles.
+ */
+function register_styles() : void {
+	register_block_style(
+		'core/heading',
+		[
+			'name' => 'report-section-heading',
+			'label' => __( 'Report Section Heading', 'wmf-reports' ),
+		]
+	);
+}
