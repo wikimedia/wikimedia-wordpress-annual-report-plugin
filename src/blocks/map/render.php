f956cec98dcc82f10eb519ed05db430f6ebeb230
Matt Watson <matt@mattwatson.codes>
2024-03-21 15:17:22 +0000

    WIP Map Front End

--- src/blocks/map/render.php
+++ src/blocks/map/render.php
@@ -2,7 +2,11 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$map_style = $attributes['mapStyle'] ?? 'mapbox://styles/mapbox/light-v11';
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
-	<div id="map"></div>
+
+<div <?php echo get_block_wrapper_attributes( [ 'class' => 'alignfull' ] ); ?>>
+	<div id="map" data-map-style="<?php echo esc_attr( $map_style ); ?>"></div>
+	<?php echo $content; ?>
 </div>
--- src/blocks/map/view.js
+++ src/blocks/map/view.js
@@ -1,34 +1,122 @@
-/**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
-// // import 'mapbox-gl/dist/mapbox-gl.css';
-// import mapboxgl from '!mapbox-gl';
+// eslint-disable-next-line import/no-unresolved
+import mapboxgl from '!mapbox-gl';
 
-// /* eslint-disable no-console */
-// console.log( 'Hello World! (from wmf-reports-example-dynamic block)' );
-// /* eslint-enable no-console */
+mapboxgl.accessToken =
+	'pk.eyJ1IjoibWF0dHdhdHNvbmhtIiwiYSI6ImNsdHpudnVmczAxdDkyaW1zYzMxNGJtbTgifQ.5UyJ9RgUB1YgoLG7vXm0aw';
 
-// mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dHdhdHNvbmhtIiwiYSI6ImNsdHpudnVmczAxdDkyaW1zYzMxNGJtbTgifQ.5UyJ9RgUB1YgoLG7vXm0aw';
-// const map = new mapboxgl.Map({
-// 	container: 'map', // container ID
-// 	center: [-74.5, 40], // starting position [lng, lat]
-// 	zoom: 9 // starting zoom
-// });
+const mapDiv = document.getElementById( 'map' );
+const fullScreenControl = new mapboxgl.NavigationControl();
+const map = new mapboxgl.Map( {
+	container: 'map',
+	center: [ -28.326915886961197, 25.093160453432485 ],
+	projection: 'equalEarth',
+	scrollZoom: false,
+	style: mapDiv?.dataset?.mapStyle || 'mapbox://styles/mapbox/light-v11',
+	zoom: 2,
+} );
+
+map.addControl( fullScreenControl );
+
+const markers = document.getElementsByClassName( 'wp-block-wmf-reports-marker' );
+
+map.on( 'load', () => {
+	// add a clustered GeoJSON source for a sample set of earthquakes
+	map.addSource( 'markers', {
+		type: 'geojson',
+		data: {
+			type: 'FeatureCollection',
+			features:
+			Array.from( markers ).map( ( marker, index ) => {
+					return {
+						geometry: {
+							type: 'Point',
+							coordinates: [
+								marker?.dataset?.long || 0,
+								marker?.dataset?.lat || 0,
+							],
+						},
+						type: 'Feature',
+						properties: {
+							id: marker?.id,
+							index,
+						},
+					};
+				} ) || [],
+		},
+		cluster: true,
+		clusterRadius: 10,
+		clusterProperties: {
+			sum: [ 'get', 'count', [ 'get', 'value', [ 'properties' ] ] ],
+		},
+	} );
+	// circle and symbol layers for rendering individual earthquakes (unclustered points)
+	map.addLayer( {
+		id: 'marker_circle',
+		type: 'circle',
+		source: 'markers',
+		filter: [ '!=', 'cluster', true ],
+		paint: {
+			'circle-color': '#000',
+			'circle-radius': 1,
+		},
+	} );
+	map.addLayer( {
+		id: 'marker_label',
+		type: 'symbol',
+		source: 'markers',
+		filter: [ '!=', 'cluster', true ],
+		layout: {
+			'text-field': '{point_count_abbreviated}',
+			'text-size': 10,
+		},
+		paint: {
+			'circle-color': '#000',
+			'text-color': 'white',
+		},
+	} );
+
+	// after the GeoJSON data is loaded, update markers on the screen on every frame
+	map.on( 'render', () => {
+		if ( ! map.isSourceLoaded( 'markers' ) ) {
+			return;
+		}
+
+		const features = map.querySourceFeatures( 'markers' );
+
+		for ( const feature of features ) {
+			const coords = feature.geometry.coordinates;
+			const { cluster, id, index } = feature.properties;
+
+			if ( ! cluster ) {
+				const markerDiv = document.createElement( 'div' );
+				markerDiv.className = 'marker';
+				markerDiv.id = id;
+
+				new mapboxgl.Marker( markerDiv )
+					.setDraggable( false )
+					.setLngLat( coords )
+					.addTo( map );
+
+				if ( index === 0 ) {
+					markerDiv.classList.add( 'active' );
+				}
+
+				markerDiv.addEventListener( 'click', () => {
+					// setCurrentItemIndex( index );
+					// selectBlock( blockId );
+				} );
+			} else {
+				const markerDiv = document.createElement( 'div' );
+				markerDiv.className = 'cluster';
+				markerDiv.id = id;
+				markerDiv.innerHTML =
+					feature.properties.point_count_abbreviated;
+
+				new mapboxgl.Marker( markerDiv )
+					.setDraggable( false )
+					.setLngLat( coords )
+					.addTo( map );
+			}
+		}
+	} );
+} );
