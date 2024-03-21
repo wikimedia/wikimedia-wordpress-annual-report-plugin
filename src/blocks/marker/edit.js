01e17f00da686a49ecab1f2e091c06a9573bcc96
Matt Watson <matt@mattwatson.codes>
2024-03-21 13:12:18 +0000

    Map Backend

---
+++ src/blocks/marker/edit.js
@@ -0,0 +1,93 @@
+import { __ } from '@wordpress/i18n';
+import {
+	InnerBlocks,
+	InspectorControls,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { BaseControl, PanelBody, TextControl } from '@wordpress/components';
+import './editor.scss';
+
+import { SearchBox } from '@mapbox/search-js-react';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @param {Object}   props               Block Props.
+ * @param {Object}   props.attributes    Block Attributes.
+ * @param {Function} props.setAttributes Set Block Attributes.
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit( { attributes, setAttributes } ) {
+	const { lat, long } = attributes;
+	return (
+		<div { ...useBlockProps() }>
+			<InspectorControls>
+				<PanelBody>
+					<BaseControl
+						help={ __(
+							'Search for the address to update the longitude and latitude',
+							'wmf-reports'
+						) }
+						id="search-box"
+						label={ __( 'Search for address', 'wmf-reports' ) }
+					>
+						<SearchBox
+							accessToken="***REMOVED***"
+							value=""
+							onRetrieve={ ( object ) => {
+								const { coordinates } = object?.features?.[ 0 ]
+									?.geometry || [
+									-13.326915886961197, 25.093160453432485,
+								];
+								setAttributes( {
+									lat: coordinates[ 1 ],
+									long: coordinates[ 0 ],
+								} );
+							} }
+						/>
+					</BaseControl>
+					<TextControl
+						help={ __(
+							'Update the latitude, use the search, or drag the marker to pin point a location.',
+							'wmf-reports'
+						) }
+						label={ __( 'Latitude', 'wmf-reports' ) }
+						value={ lat }
+						// eslint-disable-next-line no-shadow
+						onChange={ ( lat ) => {
+							setAttributes( { lat } );
+						} }
+					/>
+					<TextControl
+						help={ __(
+							'Update the longitude, use the search, or drag the marker to pin point a location.',
+							'wmf-reports'
+						) }
+						label={ __( 'Longitude', 'wmf-reports' ) }
+						value={ long }
+						// eslint-disable-next-line no-shadow
+						onChange={ ( long ) => {
+							setAttributes( { long } );
+						} }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<InnerBlocks
+				template={ [
+					[
+						'core/group',
+						{
+							align: 'full',
+							backgroundColor: 'wmf-report-bright-yellow',
+							layout: { type: 'constrained' },
+						},
+						[ [ 'core/paragraph' ] ],
+					],
+				] }
+			/>
+		</div>
+	);
+}
