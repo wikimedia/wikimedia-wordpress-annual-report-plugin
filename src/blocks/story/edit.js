64ad45e12147c9148cecca08c1fe7d9c611595d9
Matt Watson <matt@mattwatson.codes>
2024-03-24 19:55:15 +0000

    Link story to post

--- src/blocks/story/edit.js
+++ src/blocks/story/edit.js
@@ -1,8 +1,15 @@
 import { __ } from '@wordpress/i18n';
+import { createBlock } from '@wordpress/blocks';
 import {
 	InnerBlocks,
+	InspectorControls,
 	useBlockProps,
 } from '@wordpress/block-editor';
+import { Button, BaseControl, PanelBody, SelectControl } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+
+// eslint-disable-next-line import/no-unresolved
+import './editor.scss';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -10,18 +17,223 @@ import {
  *
  * @param {Object}   props               Block Props.
  * @param {Object}   props.attributes    Block Attributes.
+ * @param {number}   props.clientId      Block ClientId.
  * @param {Function} props.setAttributes Set Block Attributes.
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit( { attributes, clientId, setAttributes } ) {
+	const { PostSelectButton } = window.hm.components;
+
+	const { postId, postType } = attributes;
+
+	const childBlocks = useSelect(
+		( select ) =>
+			select( 'core/editor' ).getBlocksByClientId( clientId )[ 0 ]
+				.innerBlocks
+	);
+
+	const { media, post } = useSelect( ( select ) => {
+		// eslint-disable-next-line no-shadow
+		const post = select( 'core' ).getEntityRecord(
+			'postType',
+			postType,
+			postId
+		);
+
+		// eslint-disable-next-line no-shadow
+		const media = select( 'core' ).getMedia( post?.featured_media || 0 );
+
+		return {
+			media,
+			post,
+		};
+	} );
+
+	const { replaceBlocks } = useDispatch( 'core/block-editor' );
+
+	const onPostSelect = ( posts ) => {
+		// eslint-disable-next-line no-shadow
+		const postId = posts?.[ 0 ]?.id || 0;
+
+		if ( postId === 0 ) {
+			return;
+		}
+
+		setAttributes( { postId } );
+	};
+
+	const updateSlide = () => {
+		if ( ! postId || postId === 0 || ! post ) {
+			return;
+		}
+
+		const clientIds = childBlocks.map( ( block ) => block.clientId );
+		const heading = post.title.raw || '';
+		const imageId = post?.featured_media || 0;
+		const imageUrl = media.link;
+		const excerpt = ( post.excerpt.rendered || '' ).replace(
+			/<\/?[^>]+(>|$)/g,
+			''
+		);
+
+		console.log( 'excerpt', post.excerpt.rendered, post.excerpt, excerpt );
+
+		const newBlocks = createBlock(
+			'core/group',
+			{
+				align: 'full',
+				backgroundColor: 'wmf-report-blue',
+				className: 'carousel-slide',
+				layout: { type: 'constrained' },
+				textColor: 'base100',
+			},
+			[
+				createBlock( 'core/columns', { align: 'wide' }, [
+					createBlock( 'core/column', { width: '66.66%' }, [
+						createBlock( 'core/image', {
+							aspectRatio: '4/3',
+							className: 'is-style-default carousel-slide__image',
+							id: imageId || 74197,
+							lightbox: {
+								aspectRatio: '4/3',
+								className:
+									'is-style-default carousel-slide__image',
+								enabled: false,
+								id: imageId || 74197,
+								linkDestination: 'none',
+								scale: 'cover',
+								sizeSlug: 'full',
+								url:
+									imageUrl ||
+									'/wp-content/uploads/2024/01/Wikimedia_Foundation_AI_Blog_Series_Header.png',
+							},
+							linkDestination: 'none',
+							scale: 'cover',
+							sizeSlug: 'full',
+							url:
+								imageUrl ||
+								'/wp-content/uploads/2024/01/Wikimedia_Foundation_AI_Blog_Series_Header.png',
+						} ),
+					] ),
+					createBlock( 'core/column', { width: '33.33%' }, [
+						createBlock( 'core/heading', {
+							className:
+								'carousel-slide__heading is-style-default',
+							content:
+								heading || 'Lorem ipsum dolor sit amet vulputate.',
+						}, ),
+						createBlock( 'core/paragraph', {
+							className: 'is-style-sans-p',
+							content: excerpt ||
+								'Lorem ipsum dolor sit amet ullamcorper convallis condimentum suspendisse cras blandit congue tincidunt turpis vulputate. Imperdiet quisque libero in porta venenatis vitae netus condimentum.',
+						} ),
+						createBlock( 'core/buttons', {}, [
+							createBlock( 'core/button', {
+								backgroundColor: 'base100',
+								className:
+									'is-style-tertiary carousel-slide__button',
+								text: 'Read More',
+								style: {
+									elements: {
+										link: {
+											color: {
+												text: 'var:preset|color|base0',
+											},
+										},
+									},
+								},
+								textColor: 'base10',
+							} ),
+						] ),
+					] ),
+				] ),
+			]
+		);
+
+		replaceBlocks( clientIds, newBlocks );
+	};
+
 	return (
 		<div
 			{ ...useBlockProps( {
 				className: 'carousel__slide',
 			} ) }
 		>
+			<InspectorControls>
+				<PanelBody>
+					<SelectControl
+						label={ __( 'Choose Post Type', 'wmf-reports' ) }
+						help={ __(
+							'Altering the post type will allow you to choose posts from that post type using the select post button below.',
+							'wmf-reports'
+						) }
+						options={ [
+							{
+								label: 'Post',
+								value: 'post',
+							},
+							{
+								label: 'Page',
+								value: 'page',
+							},
+							{
+								label: 'Profile',
+								value: 'profile',
+							},
+							{
+								label: 'Report',
+								value: 'wmf-report',
+							},
+							{
+								label: 'Story (Default)',
+								value: 'story',
+							},
+						] }
+						value={ postType }
+						// eslint-disable-next-line no-shadow
+						onChange={ ( postType ) => {
+							setAttributes( { postType } );
+						} }
+					/>
+					<BaseControl
+						className="story-base-control"
+						help={ __(
+							'Associate this post with a slide. Once you have chosen a post use the button below to refresh the slide content.',
+							'wmf-reports'
+						) }
+						id={ __( 'Associated Post', 'wmf-reports' ) }
+						label={ __( 'Associated Post', 'wmf-reports' ) }
+					>
+						<PostSelectButton
+							maxPosts={ 1 }
+							postType={ postType }
+							value={ [ postId ] }
+							onSelect={ onPostSelect }
+						>
+							<span className="components-button is-secondary">
+								{ __( 'Select post', 'wmf-reports' ) }
+							</span>
+						</PostSelectButton>
+					</BaseControl>
+					{ postId !== 0 && (
+						<BaseControl
+							className="story-base-control"
+							help={ __(
+								'Pull the slide content from the associated post.',
+								'wmf-reports'
+							) }
+							id={ __( 'Refresh Slide Content', 'wmf-reports' ) }
+							label={ __( 'Refresh Slide Content', 'wmf-reports' ) }
+						>
+							<Button variant="secondary" onClick={ updateSlide }>
+								{ __( 'Refresh Slide', 'wmf-reports' ) }
+							</Button>
+						</BaseControl>
+					) }
+				</PanelBody>
+			</InspectorControls>
 			<InnerBlocks
 				template={ [
 					[
