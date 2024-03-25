05832704e2bd233b26df8d348c609612d963ca51
Matt Watson <matt@mattwatson.codes>
2024-03-25 12:09:40 +0000

    WIP Overlay

---
+++ src/blocks/overlay/view.js
@@ -0,0 +1,31 @@
+const overlays = document.getElementsByClassName(
+	'wp-block-wmf-reports-overlay'
+);
+
+Array.from( overlays ).forEach( ( overlay ) => {
+	const innerHtml = ( overlay?.innerHTML  || '' ).replace(
+		/<\/?[^>]+(>|$)/g,
+		''
+	);
+
+	if ( ! innerHtml.trim() ) {
+		return;
+	}
+
+	const parent = overlay.closest( '.wp-block-group' );
+	const button = parent.querySelector(
+		'.carousel-slide__button, overlay-trigger'
+	);
+
+	if ( ! button ) {
+		return;
+	}
+
+	button.addEventListener( 'click', ( e ) => {
+		e.preventDefault();
+
+		alert( 'TRIGGER' + innerHtml.trim() );
+
+		return false;
+	} );
+} );
