34d3bd0717b1cc9e21b849b840ec52e2f2527453
Matt Watson <matt@mattwatson.codes>
2024-03-28 10:59:22 +0000

    Style refactor

--- src/frontend.js
+++ src/frontend.js
@@ -3,17 +3,6 @@
  */
 import './frontend-global.scss';
 
-// Import specific block frontend bundles so that they hot-reload properly
-// in-editor. Going through create-block's pattern for frontend block CSS
-// reloading does not seem to work as expected.
-import './blocks/expandable/frontend.scss';
-import './blocks/table-of-contents/frontend.scss';
-
-// Import block styles.
-import './styles/core-heading-report-section-heading.scss';
-import './styles/core-group-report-overlapping-callout.scss';
-import './styles/shiro-accordion-overrides.scss';
-
 // Text formats.
 import './formats/countup-number/view';
 
