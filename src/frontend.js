976ddd4e6d6375a575a457479857959246a880ca
Matt Watson <matt@mattwatson.codes>
2024-04-03 10:52:02 +0100

    Implemented alternative accordions

--- src/frontend.js
+++ src/frontend.js
@@ -8,3 +8,6 @@ import './formats/countup-number/view';
 
 // Frontend logic that isn't tied to a custom block.
 import './features/core-group-report-overlapping-callout';
+
+// Open accordion first items by default.
+import './features/accordion';
