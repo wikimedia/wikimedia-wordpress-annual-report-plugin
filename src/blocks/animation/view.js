63049bac6290d84734187e0fa85f53d816eef04f
K. Adam White <kadamwhite@users.noreply.github.com>
2024-10-14 16:11:48 +0900

    1040: Refactor IntersectionObserver usage in Animation block for clarity

--- src/blocks/animation/view.js
+++ src/blocks/animation/view.js
@@ -1,12 +1,51 @@
 import lottie from 'lottie-web';
 
+const animations = new Map();
+
+const observer = new window.IntersectionObserver(
+	( entries ) => {
+		entries.forEach( ( entry ) => {
+			const animation = animations.get( entry.target );
+			if ( ! animation ) {
+				return;
+			}
+			if ( ! entry.isIntersecting ) {
+				if ( ! entry.isVisible ) {
+					animation.setDirection( -1 );
+					animation.play();
+				}
+				return;
+			}
+			animation.setDirection( 1 );
+			animation.play();
+		} );
+	},
+	{
+		root: null,
+		threshold: 1, // set offset 0.1 means trigger if at least 10% of element in viewport
+	}
+);
+
+/**
+ * Register an animation with our intersection observer.
+ *
+ * @param {HTMLDivElement}                     container Animation container element.
+ * @param {import('lottie-web').AnimationItem} animation Lottie animation instance.
+ */
+function registerAnimation( container, animation ) {
+	animations.set( container, animation );
+
+	observer.observe( container );
+}
+
 document
 	.querySelectorAll( '.wmf-animation[data-animation]' )
+	/**
+	 * Initialize all animations.
+	 *
+	 * @param {HTMLDivElement} animationContainer Animation container element.
+	 */
 	.forEach( ( animationContainer ) => {
-		console.log( {
-			animationContainer,
-			animationData: animationContainer.dataset.animation ?? '',
-		} );
 		let animationData = '';
 		try {
 			animationData = JSON.parse( animationContainer.dataset.animation );
@@ -29,23 +68,5 @@ document
 			animationData,
 		} );
 
-		const observer = new window.IntersectionObserver(
-			( [ entry ] ) => {
-				if ( ! entry.isIntersecting ) {
-					if ( ! entry.isVisible ) {
-						animation.setDirection( -1 );
-						animation.play();
-					}
-					return;
-				}
-				animation.setDirection( 1 );
-				animation.play();
-			},
-			{
-				root: null,
-				threshold: 1, // set offset 0.1 means trigger if at least 10% of element in viewport
-			}
-		);
-
-		observer.observe( animationContainer );
+		registerAnimation( animationContainer, animation );
 	} );
