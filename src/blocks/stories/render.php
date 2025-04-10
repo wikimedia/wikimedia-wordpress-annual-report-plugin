b45f79a0f62c18b273473889d442a1703d84fcac
goldenapples <goldenapplesdesign@gmail.com>
2025-04-10 11:55:43 -0400

    Allow "stories" carousel block to be used more than once on a page

--- src/blocks/stories/render.php
+++ src/blocks/stories/render.php
@@ -49,16 +49,16 @@ $cssColors = array_map( function( $context, $color_slug ) {
 		</div>
 	</div>
 	<div class="stories__categories-buttons alignwide">
-		<button id="carousel-slide-back" class="carousel__button carousel__button--back"><span class="screen-reader-text"><?php esc_html_e( 'Previous Story', 'wmf-reports' ); ?></span></button>
-		<button id="carousel-slide-forward" class="carousel__button carousel__button--forward"><span class="screen-reader-text"><?php esc_html_e( 'Next Story', 'wmf-reports' ); ?></span></button>
+		<button class="carousel__button carousel__button--back"><span class="screen-reader-text"><?php esc_html_e( 'Previous Story', 'wmf-reports' ); ?></span></button>
+		<button class="carousel__button carousel__button--forward"><span class="screen-reader-text"><?php esc_html_e( 'Next Story', 'wmf-reports' ); ?></span></button>
 	</div>
 	<div class="carousel-wrapper">
 		<?php echo $content; ?>
 		<?php if ( substr_count( $content, 'wp-block-wmf-reports-story' ) > 1 ) : ?>
 		<div class="carousel__buttons-wrapper">
 			<div class="carousel__buttons alignwide">
-				<button id="carousel-back" class="carousel__button carousel__button--back"><span class="screen-reader-text"><?php esc_html_e( 'Previous Story', 'wmf-reports' ); ?></span></button>
-				<button id="carousel-forward" class="carousel__button carousel__button--forward"><span class="screen-reader-text"><?php esc_html_e( 'Next Story', 'wmf-reports' ); ?></span></button>
+				<button class="carousel__button carousel__button--back"><span class="screen-reader-text"><?php esc_html_e( 'Previous Story', 'wmf-reports' ); ?></span></button>
+				<button class="carousel__button carousel__button--forward"><span class="screen-reader-text"><?php esc_html_e( 'Next Story', 'wmf-reports' ); ?></span></button>
 			</div>
 		</div>
 		<?php endif; ?>
